d60bf1cd37098677c9a5f8acae285aed375d2dd5
Alex Boylan <aboylan@users.noreply.github.com>
2023-07-31 14:33:49 -0600

    Anotacion @ModelAttribute

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
@@ -36,13 +37,18 @@ public class IndexController {
 
 	@RequestMapping("/listar")
 	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado de usuarios");
+
+		return "listar";
+	}
+
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarusuarios() {
 		List<Usuario> usuarios = Arrays.asList(new Usuario("Alejandro", "Boylan", "aboylan@correo.com"),
 				new Usuario("Jophn", "Doe", "john@correo.com"), new Usuario("Jane", "Doe", "jane@correo.com"),
 				new Usuario("Tornado", "Roe", "roe@correo.com"));
 
-		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
-
-		return "listar";
+		return usuarios;
 	}
+
 }
